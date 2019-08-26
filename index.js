4216cec8b07b1f5225ddf3ff5e50abd8a7b5adad
Nick Spera <nickaspera@gmail.com>
2019-08-26 15:02:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
+}
 
 function deleteFromObjectByKey(object, key){
   object[key] = value;
