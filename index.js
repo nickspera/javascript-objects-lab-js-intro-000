f4ffff20c0712f088781e733a328b59860af79af
Nick Spera <nickaspera@gmail.com>
2019-08-26 15:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var recipes = {}
 
-// function updateObjectWithKeyAndValue(object, key, value){
-//   recipes.prop = 'new value';
-//   return recipes;
-// }
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value });
 }
 
-
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value;
 
 function deleteFromObjectByKey(object, key){
-  object[key] = value
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes.prop = 'new value';
- return recipes;
-}
+  object[key] = value;
+  }
