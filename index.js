8989b874e05473390a181675e6dcee33512dfa1c
Nick Spera <nickaspera@gmail.com>
2019-08-26 14:55:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var recipes = {
 //   return recipes;
 // }
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, recipes, { [prop]: 'new value' });
+  return Object.assign({}, object, { [prop]: 'new value' });
   return recipes;
 }
 
